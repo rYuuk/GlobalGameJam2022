f8c4e7814465f1588740959e69e5e22f9fb0daf2
Robin <robin.sharma@innerspace.eu>
2022-01-29 00:20:57 +0100

    Add main menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameObject creditPanel;
+
+    [SerializeField] private Button continueButton;
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button creditButton;
+    [SerializeField] private Button quitButton;
+
+    [SerializeField] private Button backButton;
+
+    private void OnEnable()
     {
-        
+        startButton.onClick.AddListener(StartGame);
+        continueButton.onClick.AddListener(Continue);
+        creditButton.onClick.AddListener(Credit);
+        quitButton.onClick.AddListener(Quit);
+        backButton.onClick.AddListener(Back);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        startButton.onClick.RemoveListener(StartGame);
+        continueButton.onClick.RemoveListener(Continue);
+        creditButton.onClick.RemoveListener(Credit);
+        quitButton.onClick.RemoveListener(Quit);
+        backButton.onClick.RemoveListener(Back);
+    }
+
+    private void StartGame()
+    {
+        menuPanel.gameObject.SetActive(false);
+    }
+
+    private void Continue()
+    {
+        menuPanel.gameObject.SetActive(false);
+    }
+
+    private void Credit()
+    {
+        creditPanel.gameObject.SetActive(true);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void Back()
+    {
+        creditPanel.SetActive(false);
     }
 }
