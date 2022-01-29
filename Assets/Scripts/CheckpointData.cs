256b528bba5f1317a3e2717b4fb41da165d707c3
Robin <robin.sharma@innerspace.eu>
2022-01-29 18:21:29 +0100

    Clean up

--- Assets/Scripts/CheckpointData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObjects/GameData", order = 1)]
-public class CheckpointData : ScriptableObject
-{
-    public int lastCheckpointID;
-}
