f2ff7930e5d93818a4ff31ef9b295b069f93f801
Miro Kroslin <miro.kroslin@innerspace.eu>
2022-01-29 15:01:20 +0100

    rough Checkpoints, Prism explosion and more

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using VContainer;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Checkpoint : MonoBehaviour
+{
+    [Inject] private CharacterController player;
+    [Inject] private GameData data;
+
+    private const string PlayerTag = "Player";
+
+    private void Awake()
+    {
+    }
+
+    private void MovePlayerToCheckpoint()
+    {
+        player.transform.position = transform.position;
+        player.Reset();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(PlayerTag))
+        {
+            // Subscribe to player death here -> SpawnNewPlayer
+        }
+    }
+}
