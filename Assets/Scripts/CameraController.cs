83d25ccc8bf7f42dc0c5c840a4805a0d478657e6
Robin <robin.sharma@innerspace.eu>
2022-01-30 15:08:52 +0100

    Add interpolate in player rigidbody

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,24 +1,24 @@
 using System;
 using DG.Tweening;
 using UnityEngine;
+using VContainer;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset;
     [SerializeField] private float speed;
 
+    [Inject] private Player player;
+
+    private Transform target;
+
     private void Awake()
     {
-        if (target==null)
-        {
-            target = FindObjectOfType<Player>().transform;
-        }
+        target = FindObjectOfType<Player>().transform;
     }
 
     private void Update()
     {
         transform.position = Vector3.Slerp(transform.position, target.position + offset, Time.deltaTime * speed);
-        //transform.DOMove(target.position + offset, Time.deltaTime * speed);
     }
 }
